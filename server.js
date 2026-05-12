6e76aa5705daf77538736e116da2abd375b123f9
zeus <zeus@zeus>
2026-05-12 10:06:02 +0200

    fix: adding prod/express

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+const distPath = path.join(__dirname, 'dist/automata');
+
+app.use(express.static(distPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(distPath, 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
